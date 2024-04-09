57a2e448e480e223fe0deca96f451e29854e2177
Tim King <kingtimm@gmail.com>
2024-04-08 21:21:52 -0500

    same app

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,12 @@
-<template>
-    <div class="max-w-2xl mx-auto">
-        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Taxing Laughter: The Joke Tax Chronicles
-        </h1>
-        <p class="leading-7 [&:not(:first-child)]:mt-6">
-            Once upon a time, in a far-off land, there was a very lazy king who
-            spent all day lounging on his throne. One day, his advisors came to him
-            with a problem: the kingdom was running out of money.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem esse consectetur optio quis quaerat, eum
-            blanditiis asperiores nihil, quibusdam repellendus reiciendis ut exercitationem sunt voluptates eaque porro,
-            hic quod adipisci.
-        </p>
-        <Button variant="default" @click="console.log('test')">Test</Button>
-    </div>
-</template>
-
 <script setup lang="ts">
-import { Button } from '~/components/ui/button'
+import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+
+const count = ref(0)
 </script>
+
+<template>
+  <Button type="button" @click="count++">
+    count is {{ count }}
+  </Button>
+</template>
