c7037e6418be1a54e8222f62bd7faf0afa5ef921
Tim King <kingtimm@gmail.com>
2024-04-10 14:25:02 -0500

    fix: added colorscheme

--- src/app.vue
+++ src/app.vue
@@ -7,34 +7,36 @@ const count = ref(0)
 </script>
 
 <template>
-  <div class="p-3 space-y-4">
-    <div class="flex items-center justify-between">
-      <h1 class="text-xl">
-        Starter Nuxt Unocss Shadcn
-      </h1>
-      <div class="flex gap-2 items-center">
-        <Button variant="outline">
-          <NuxtLink
-            to="https://github.com/kingtimm/uno-shadcn-nuxt-src"
-            target="_blank"
-          >
-            <Icon icon="radix-icons:github-logo" />
-          </NuxtLink>
-        </Button>
-        <ColorSwitcher />
+  <ColorScheme>
+    <div class="p-3 space-y-4">
+      <div class="flex items-center justify-between">
+        <h1 class="text-xl">
+          Starter Nuxt Unocss Shadcn
+        </h1>
+        <div class="flex gap-2 items-center">
+          <Button variant="outline">
+            <NuxtLink
+              to="https://github.com/kingtimm/uno-shadcn-nuxt-src"
+              target="_blank"
+            >
+              <Icon icon="radix-icons:github-logo" />
+            </NuxtLink>
+          </Button>
+          <ColorSwitcher />
+        </div>
       </div>
+      <div class="space-y-4">
+        <p>
+          Reactive
+        </p>
+        <Button
+          type="button"
+          @click="count++"
+        >
+          count is {{ count }}
+        </Button>
+      </div>
+      <ThemeSwitcher />
     </div>
-    <div class="space-y-4">
-      <p>
-        Reactive
-      </p>
-      <Button
-        type="button"
-        @click="count++"
-      >
-        count is {{ count }}
-      </Button>
-    </div>
-    <ThemeSwitcher />
-  </div>
+  </ColorScheme>
 </template>
